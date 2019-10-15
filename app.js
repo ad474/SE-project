bec25aa04e91b7606f46a8150d14e017b584ed9c
Ankita Durgavajula <35971200+ad474@users.noreply.github.com>
2019-10-15 13:44:28 +0530

    set up ejs

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 //jshint esversion:6
 
 const express= require("express");
+const bodyParser= require("body-parser");
 
 const app= express();
 app.set('view engine', 'ejs');
 
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.send("Hello World");
+  res.render('index');
 });
 
 app.listen(3000, function(){
